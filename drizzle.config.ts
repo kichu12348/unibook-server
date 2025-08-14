097c8dbc1fd7a8038d100b4626a68b8544f80ab0
Mahadevan Reji <rmahadevan574@gmail.com>
2025-08-14 21:59:40 +0530

    bel

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    ssl:{
+      rejectUnauthorized: false,
+    }
   },
 
   verbose: true,
