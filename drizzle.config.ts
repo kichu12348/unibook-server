8e58c8a93ef289818d315451dfd04bb850a9bfdf
Mahadevan Reji <rmahadevan574@gmail.com>
2025-08-14 21:53:46 +0530

    back

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,9 +9,6 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
-    ssl: {
-      rejectUnauthorized: false,
-    },
   },
 
   verbose: true,
