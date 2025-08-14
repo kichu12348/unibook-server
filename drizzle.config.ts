dfc9975159696abcc0445f6f24cb898e75fdb23c
Mahadevan Reji <rmahadevan574@gmail.com>
2025-08-14 21:53:12 +0530

    fix: Update SSL configuration in drizzle.config.ts for database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,7 +9,11 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
+
   verbose: true,
   strict: true,
 });
