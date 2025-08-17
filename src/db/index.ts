c4a4df4a07a0e84144cf064456217f04725cf0f6
Mahadevan Reji <rmahadevan574@gmail.com>
2025-08-17 18:26:12 +0530

    hmm

--- src/db/index.ts
+++ src/db/index.ts
@@ -14,9 +14,9 @@ if (!connectionString) {
 
 // Create the connection
 const client = postgres(connectionString,{
-  // ssl:{
-  //   rejectUnauthorized: false
-  // }
+  ssl:{
+    rejectUnauthorized: false
+  }
 });
 
 // Create the database instance
